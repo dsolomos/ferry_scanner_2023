cb928f279ab18078c5328818f496fe16fa1f25c6
Dimitrios Solomos <solomos7@live.com>
2023-06-05 19:45:18 +0100

    Adding the first model - FerryOption

--- ferry_scanner_app/scanner/models.py
+++ ferry_scanner_app/scanner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class FerryOption(models.Model):
+    departure = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    date = models.DateField()
+    seat_type = models.CharField(max_length=100)
+    availability = models.CharField(max_length=100) 
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f'{self.departure} to {self.destination} - {self.date}'
